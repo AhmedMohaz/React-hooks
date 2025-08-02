76512b3157f22c1a72ac34aad760a2e5753c72e4
Ahmed Mohamed <itslokin5@gmail.com>
2025-08-02 12:13:36 +0300

    Create moviesData.js

---
+++ src/moviesData.js
@@ -0,0 +1,33 @@
+const moviesData = [
+  {
+    title: "Inception",
+    description: "A thief who steals corporate secrets through dream-sharing tech.",
+    posterURL: "https://m.media-amazon.com/images/I/51s+7rN7gGL._AC_SY679_.jpg",
+    rating: 5
+  },
+  {
+    title: "Interstellar",
+    description: "A group of explorers travel through a wormhole in space.",
+    posterURL: "https://m.media-amazon.com/images/I/71niXI3lxlL._AC_SY679_.jpg",
+    rating: 4
+  }
+];
+
+export default moviesData;
+
+const moviesData = [
+  {
+    title: "Inception",
+    description: "A thief who steals corporate secrets through dream-sharing tech.",
+    posterURL: "https://m.media-amazon.com/images/I/51s+7rN7gGL._AC_SY679_.jpg",
+    rating: 5
+  },
+  {
+    title: "Interstellar",
+    description: "A group of explorers travel through a wormhole in space.",
+    posterURL: "https://m.media-amazon.com/images/I/71niXI3lxlL._AC_SY679_.jpg",
+    rating: 4
+  }
+];
+
+export default moviesData;
